baa18de43b416c3b5557e6182fc191817ea34047
kinsigne <kiminsigne@gmail.com>
2018-02-28 08:30:51 -0800

    updating supplement scripts

---
+++ scripts/supplement/fig1D_ext_fig4EF_mutation_categories.R
@@ -0,0 +1,222 @@
+###############################################################################
+# set-up
+###############################################################################
+load_pkgs <- function(pkgs){
+  new_pkgs <- pkgs[!(pkgs %in% installed.packages()[, 'Package'])]
+  if(length(new_pkgs)) install.packages(new_pkgs)
+  for(pkg in pkgs){
+    suppressWarnings(suppressMessages(library(pkg, character.only = T)))
+  }
+}
+
+pkgs <- c('dplyr', 'tidyr', 'ggplot2', 'cowplot', 'gridExtra', 'grid')
+load_pkgs(pkgs)
+
+options(stringsAsFactors = F, warn = -1, warnings = -1)
+
+plot_format_main <- '.tiff'
+plot_format <- '.tiff'
+hi_res <- 600
+jitter_alpha <- 0.10
+
+###############################################################################
+# Read in data
+###############################################################################
+data <- read.table('../../processed_data/sre/sre_data_clean.txt', 
+                   sep = '\t', header = T, 
+                   colClasses = c('sub_id' = 'character')) %>% 
+  filter(rep_quality == 'high')
+
+# read in re-scored reference file
+updated_ref <- read.csv('../../ref/sre/sre_ref_rescored.txt',
+                        header = T, sep = '\t',
+                        colClasses = c('sub_id' = 'character'))
+
+data <- data %>% 
+  left_join(dplyr::select(updated_ref, id, exon_seq:correct_don_score), by = 'id')
+
+###############################################################################
+# sre categories
+###############################################################################
+
+esr_categories <- c('rmv_Ke2011_ESE', 'clst_Ke2011_ESE', 'rmv_Ke2011_ESS', 
+                    'clst_Ke2011_ESS')
+esr_labels <- c('weaken ESEs', 'destroy strongest ESE', 'weaken ESSs', 
+                'destroy strongest ESS')
+
+splice_site_categories <- c( 'weak_spl_a', 'weak_spl_d', 'p_weak_spl', 
+                             'no_spl_a', 'no_spl_d',
+                             'same_splice_a', 'same_splice_d',
+                             'rmv_me_splice_acceptor', 'rmv_me_splice_donor', 
+                             'csplice_a', 'csplice_d')
+splice_site_labels <- c('weaken acceptor', 'weaken donor', 
+                        'weaken donor + acceptor', 
+                        'destroy acceptor', 'destroy donor',
+                        'same score acceptor', 'same score donor',
+                        'weaken spurious acceptor', 'weaken spurious donor',
+                        'destroy spurious acceptor', 'destroy spurious donor')
+
+intron_categories <- c('clst_Vlkr07_AICS', 'clst_Vlkr07_DICS', 
+                       'rmv_Vlkr07_AICS', 'rmv_Vlkr07_DICS')
+intron_labels <- c('weaken intronic conserved (acceptor side)', 
+                   'weaken intronic conserved (donor side)', 
+                   'destroy intronic conserved (acceptor side)', 
+                   'destroy intronic conserved (donor side)')
+
+random_exon_categories <- c('rnd_exon_1nt', 'rnd_exon_2nt', 
+                            'rnd_exon_3nt', 'rnd_exon_5nt', 
+                            'aggr_exon')
+random_exon_labels <- c('random 1nt exon', 'random 2nt exon', 
+                        'random 3nt exon', 'random 5nt exon',
+                        'aggressive exon (only syn. mut.)')
+
+random_intron_categories <- c('rnd_intron_1nt', 'rnd_intron_2nt', 
+                              'rnd_intron_3nt', 'rnd_intron_5nt', 
+                              'aggr_intron', 'p_aggr_intr', 'aggr_both')
+random_intron_labels <- c('random 1nt intron', 'random 2nt intron', 
+                          'random 3nt intron', 'random 5nt intron',
+                          'aggressive intron', 'aggr. + random intron', 
+                          'aggr. intron + exon')
+
+other_categories <- c('cnsrv_1nt', 'cnsrv_3nt', 
+                      'RBPmats', 'variation')
+other_labels <- c('conserved 1nt', 'conservered 3nt', 
+                  'destroy RBP motifs', 'dbSNPs')
+
+# SMN1 intron backbone INCLUDED
+gg <- data %>% 
+  filter(seq_type == 'mut', nat_index_smn1 >= 0.50) %>% 
+  mutate(category_fctr = factor(category,
+                                levels = c(splice_site_categories, 
+                                           esr_categories, 
+                                           random_exon_categories,
+                                           intron_categories, 
+                                           random_intron_categories, 
+                                           other_categories))) %>% 
+  ggplot(aes(x = category_fctr, y = dpsi_smn1)) + 
+  geom_jitter(alpha = jitter_alpha, size = 0.5, color = 'red') + 
+  geom_boxplot(alpha = 0) +
+  geom_hline(yintercept = -0.5, linetype = 'dashed') +
+  scale_x_discrete(labels = c(splice_site_labels, esr_labels, 
+                              random_exon_labels, intron_labels, 
+                              random_intron_labels, other_labels)) +
+  theme(axis.title.y = element_text(size = 18, vjust = 1),
+        axis.text.x = element_text(angle = 45, hjust = 1, color = "grey20"),
+        axis.text.y = element_text(size = 10, color = "grey20"),
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_line(color = "grey50"),
+        axis.line.x = element_line(color = "grey50"),
+        axis.line.y = element_line(color = "grey50"),
+        legend.position = 'none') +
+  labs(x = '', y = expression(paste(Delta, ' inclusion index')),
+       color = expression(index["WT "]))
+
+gg_no_x_axis <- gg + theme(axis.text.x = element_blank())
+
+
+ggsave(paste0('../../figs/sre/smn1/', 
+              'ext_fig4E_smn1_all_categories_no_x_axis_INCLUDED', plot_format_main), 
+       gg_no_x_axis, width = 12, height = 3, dpi = hi_res)
+
+
+# SMN1 intron backbone EXCLUDED
+gg <- data %>% 
+  filter(seq_type == 'mut', nat_index_smn1 < 0.5) %>% 
+  mutate(category_fctr = factor(category,
+                                levels = c(splice_site_categories, 
+                                           esr_categories, 
+                                           random_exon_categories,
+                                           intron_categories, 
+                                           random_intron_categories, 
+                                           other_categories))) %>% 
+  ggplot(aes(x = category_fctr, y = dpsi_smn1)) + 
+  geom_jitter(alpha = jitter_alpha, size = 0.5, color = 'blue') + 
+  geom_boxplot(alpha = 0) +
+  scale_x_discrete(labels = c(splice_site_labels, esr_labels, 
+                              random_exon_labels, intron_labels, 
+                              random_intron_labels, other_labels)) +
+  theme(axis.title.y = element_text(size = 18, vjust = 1),
+        axis.text.x = element_text(angle = 45, hjust = 1, color = "grey20"),
+        axis.text.y = element_text(size = 10, color = "grey20"),
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_line(color = "grey50"),
+        axis.line.x = element_line(color = "grey50"),
+        axis.line.y = element_line(color = "grey50"),
+        legend.position = 'none') +
+  labs(x = '', y = expression(paste(Delta, ' inclusion index')),
+       color = expression(index["WT "]))
+
+gg_no_x_axis <- gg + theme(axis.text.x = element_blank())
+
+ggsave(paste0('../../figs/sre/smn1/', 
+              'ext_fig4F_smn1_all_categories_no_x_axis_EXCLUDED', plot_format), 
+       gg_no_x_axis, width = 12, height = 3, dpi = 100)
+
+# percent splice-disrupting mutants by category
+sdv_by_category_smn1 <- data %>% 
+  filter(nat_index_smn1 >= 0.75) %>%
+  mutate(sdv_smn1 = ifelse(dpsi_smn1 <= -0.50, T, F)) %>% 
+  group_by(category) %>% 
+  summarise(num_sdv_smn1 = length(which(sdv_smn1 == T)),
+            category_num_smn1 = n()) %>% 
+  mutate(percent_sdv_smn1 = num_sdv_smn1/ category_num_smn1) %>%
+  arrange(desc(num_sdv_smn1)) %>%
+  ungroup()
+
+# percent splice-disrupting mutants by category
+snv_by_category_smn1 <- data %>% 
+  filter(nat_index_smn1 >= 0.75) %>%
+  mutate(snv_smn1 = ifelse(num_changes == 1, T, F)) %>% 
+  group_by(category) %>% 
+  summarise(num_snv_smn1 = length(which(snv_smn1 == T)),
+            category_num_smn1 = n()) %>% 
+  # mutate(percent_sdv_smn1 = num_sdv_smn1/ category_num_smn1) %>%
+  arrange(desc(num_snv_smn1)) %>%
+  ungroup()
+
+sdv_snv_by_category_smn1 <- data %>% 
+  filter(nat_index_smn1 >= 0.75, num_changes == 1) %>%
+  mutate(sdv_smn1 = ifelse(dpsi_smn1 <= -0.50, T, F)) %>% 
+  group_by(category) %>% 
+  summarise(num_sdv_smn1 = length(which(sdv_smn1 == T)),
+            category_num_smn1 = n()) %>% 
+  mutate(percent_sdv_smn1 = num_sdv_smn1/ category_num_smn1) %>%
+  arrange(desc(num_sdv_smn1)) %>%
+  ungroup()
+
+gg <- data %>% 
+  filter(nat_index_smn1 >= 0.75, !is.na(category)) %>%
+  mutate(sdv_smn1 = ifelse(dpsi_smn1 <= -0.50, T, F)) %>% 
+  mutate(category_fctr = factor(category,
+                                levels = c(splice_site_categories, 
+                                           esr_categories, 
+                                           random_exon_categories,
+                                           intron_categories, 
+                                           random_intron_categories, 
+                                           other_categories))) %>%
+  group_by(category_fctr) %>% 
+  summarise(num_sdv_smn1 = length(which(sdv_smn1 == T)),
+            category_num_smn1 = n()) %>% 
+  mutate(percent_sdv_smn1 = num_sdv_smn1/ category_num_smn1 * 100) %>%
+  ggplot(aes(x = category_fctr, y = percent_sdv_smn1)) +
+  geom_col(alpha = 1, width = 0.8, fill = "blue") +
+  scale_y_continuous(expand = c(0,0)) +
+  expand_limits(y = 105) +
+  scale_x_discrete(labels = c(splice_site_labels, esr_labels, 
+                              random_exon_labels, intron_labels, 
+                              random_intron_labels, other_labels)) +
+  theme(axis.title.y = element_text(size = 18, vjust = 1),
+        axis.text.x = element_text(angle = 45, hjust = 1, color = "grey20"),
+        axis.text.y = element_text(size = 8, color = "grey20"),
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_line(color = "grey50"),
+        axis.line.x = element_line(color = "grey50"),
+        axis.line.y = element_line(color = "grey50"),
+        legend.position = 'none') +
+  labs(x = '', y = "% SDV")
+
+gg_no_x_axis <- gg + theme(axis.text.x = element_blank())
+
+ggsave(paste0('../../figs/sre/smn1/', 
+              'fig1D_smn1_all_categories_percent_sdv', plot_format), 
+       gg_no_x_axis, width = 12, height = 3, dpi = 100)
